3c466a8e7302c4f727618599ffd36226d7d05fa0
SergeyKakusha <127593819+SergeyKakusha@users.noreply.github.com>
2024-03-24 22:56:32 +0200

    d

--- Lesson1-script.js
+++ Lesson1-script.js
@@ -710,16 +710,16 @@
 // // задача з Д/з 
 
 
-// function getElementWidth(content, padding, border) {
-//     return Number.parseFloat(content)+Number.parseFloat(padding)*2+Number.parseFloat(border)*2
-// }
+function getElementWidth(content, padding, border) {
+    return Number.parseFloat(content)+Number.parseFloat(padding)*2+Number.parseFloat(border)*2
+}
 
 
-// console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
 
 
 
-// // Lesson1-script.js:718 101
+// Lesson1-script.js:718 101
 
 
 
